a593f6952c2d4a4f325b3cd18ebc93d4e9045077
Tony Betts <tonybetts@feroscare.com.au>
2021-12-16 08:01:02 +1000

    Add projects

---
+++ UnitTests/SetupTests.cs
@@ -0,0 +1,14 @@
+using NUnit.Framework;
+
+namespace UnitTests
+{
+    public class SetupTests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+
+    }
+}
---
+++ UnitTests/SortingAlgos/Tests.cs
@@ -0,0 +1,19 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnitTests.SortingAlgos
+{
+    [TestFixture]
+    public class Tests
+    {
+        [Test]
+        public void PracTest()
+        {
+            Assert.AreEqual(0, 0);
+        }
+    }
+}
