b3529ed1c80443c08c77ad0f58fbcf652c0dd03c
Tony Betts <tonybetts@feroscare.com.au>
2021-12-16 22:55:24 +1000

    Stack implemented with System.Array and tests

---
+++ CSharpDataStructures/MyStack.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpDataStructures
+{
+
+    public class MyStack<T>
+    {
+        public T?[] Stack { get; private set; }
+        public int MaxStackIndex { get; private set; }
+        public int Head { get; private set; } 
+        public MyStack(int stackSize)
+        {
+            Stack = new T[stackSize];
+            MaxStackIndex = stackSize - 1;
+            Head = -1; // Initally we are not in the first  i.e 0 index (only when we do our first push
+        }
+
+        public T? Pop()
+        {
+            var any = AnyStackItems();
+            if (!any)
+                throw new ArgumentOutOfRangeException("Stack is empty, Why did you try access me man!!!!");
+
+            var returnVal = Stack[Head];
+            Head--; 
+
+            return returnVal;
+            
+        }
+
+        public void Push(T? pushValue)
+        {
+            if (Head == MaxStackIndex)
+                throw new ArgumentOutOfRangeException($"Stack is full bra. You have reached your max size {MaxStackIndex + 1}");
+            
+            Head = Head + 1;
+            Stack[Head] = pushValue;
+            
+            
+        }
+
+        public bool IsEmpty()
+        {
+            return !AnyStackItems();
+        }
+
+        public T? Peek()
+        {
+            return AnyStackItems() switch
+            {
+                false => throw new ArgumentOutOfRangeException("Stack is empty, Why did you try access me man!!!!"),
+                true => Stack[Head] 
+            };
+        }
+
+        public bool AnyStackItems()
+        {
+            return Head switch
+            {
+                < 0 => false,
+                _ => true
+            };
+        }
+
+
+    }
+}
---
+++ UnitTests/DataStructures/StackTests.cs
@@ -0,0 +1,86 @@
+﻿using CSharpDataStructures;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnitTests.DataStructures
+{
+    [TestFixture]
+    public class StackTests
+    {
+        [Test]
+        public void IsEmptyTest()
+        {
+            var stack = new MyStack<int>(1);
+            var sut = stack.IsEmpty();
+            Assert.IsTrue(sut);
+            stack.Push(1);
+            var sut2 = stack.IsEmpty();
+            Assert.IsFalse(sut2);
+        }
+
+        [Test]
+        public void PopTest()
+        {
+            var stack = new MyStack<int>(2);
+            var expected = 4;
+            stack.Push(expected);
+            var sut = stack.Pop();
+            Assert.AreEqual(expected, sut); 
+        }
+        [Test]
+        public void PeekTest()
+        {
+            var stack = new MyStack<int>(3);
+            var expected = 4;
+            stack.Push(expected);
+            var sut = stack.Peek();
+            Assert.AreEqual(expected, sut);
+        }
+
+        [Test]
+        public void PopExceptionTest()
+        {
+            var stack = new MyStack<int>(3);
+            var didThrow = false;
+            try
+            {
+                stack.Pop();
+            }
+            catch(ArgumentOutOfRangeException ex)
+            {
+                didThrow = true;    
+            }
+            finally
+            {
+                Assert.IsTrue(didThrow);
+            }
+            
+        }
+
+        [Test]
+        public void PushExceptionTest()
+        {
+            var stack = new MyStack<int>(2);
+            var didThrow = false;
+            stack.Push(1);
+            stack.Push(2);
+            try
+            {
+                stack.Push(3);
+            }
+            catch (ArgumentOutOfRangeException ex)
+            {
+                didThrow= true;
+            }
+            finally
+            {
+                Assert.IsTrue(didThrow);
+            }
+
+        }
+    }
+}
